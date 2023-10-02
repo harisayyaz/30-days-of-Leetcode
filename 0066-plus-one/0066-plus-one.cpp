10ecc02072239d14c235ddfbb9562432e81dad6d
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-03 02:12:01 +0500

    Time: 0 ms (100%), Space: 9.1 MB (8.38%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size()-1 ; i >= 0; i--)
+    {
+         if (digits.at(i)!=9)
+         {
+            digits.at(i) += 1;
+            return digits;
+        }
+        digits.at(i) = 0;
+    }
+    if (digits.at(0)==0)
+        {       
+         digits.insert (digits.begin(), 1);
+        }
+    return digits;
+    }
+};
