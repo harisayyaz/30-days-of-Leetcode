c03aebccf2b32c7eae33061fb26b3066fbf77a63
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-04 01:08:34 +0500

    Time: 3 ms (81.86%), Space: 10.7 MB (6.11%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -6,18 +6,16 @@ int arraySign(vector<int> &nums)
     int product = 1;
     for (int i = 0; i < nums.size(); i++)
     {
-        if (nums.at(i) == 0)
+    if (nums.at(i) == 0)
     {
         return 0;
     }
-    else if (product*nums.at(i) > 0)
+    if (product*nums.at(i) > 0)
     {
         product = 1;
+        continue;
     }
-    else
-    {
-        product =-1;
-    }
+    product = -1;
     }
     return product;
 }
