9dfc3da2be77044a1f594b896eb03e24dbe7fc7f
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-04 02:05:40 +0500

    Time: 0 ms (100%), Space: 43.1 MB (41.57%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -1,7 +1,8 @@
 class Solution {
     public int arraySign(int[] nums) {
     short countOfNeg = 0;
-    for (int i = 0; i<nums.length; i++){
+    int len = nums.length;
+    for (int i = 0; i<len; i++){
     if (nums[i] == 0) return 0;
     if (nums[i] < 0) countOfNeg++;
     }
