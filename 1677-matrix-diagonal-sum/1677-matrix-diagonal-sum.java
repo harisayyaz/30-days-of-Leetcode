86fb5613bb7ed462eff8d2b6c723199fe5cf0852
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-05 11:10:10 +0500

    Time: 0 ms (100%), Space: 43.7 MB (73.04%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -4,7 +4,7 @@ class Solution {
         int endPointer = size-1;
         int sum = 0;
         for (int i = 0; i < size; i++) {
-            sum += (mat[i][i] + mat[i][endPointer--]);
+            sum += (mat[i][i] + mat[i][size-i-1]);
         }
         return ((size%2==0) ? sum : sum-mat[size/2][size/2]);
     }
