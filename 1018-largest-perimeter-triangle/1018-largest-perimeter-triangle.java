b209f0f49aecb644b874046c583a286d804148f6
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-10 23:29:26 +0500

    Time: 13 ms (10.98%), Space: 44.1 MB (76.77%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -1,6 +1,6 @@
 class Solution {
    public static int largestPerimeter(int[] nums) {
-        bubbleSort(nums);
+        sort(nums, 0, nums.length-1);
         for (int i = nums.length-1; i >= 2; i--) {
             if (area(nums[i], nums[i-1], nums[i-2])) {
                 return nums[i]+nums[i-1]+nums[i-2];
@@ -9,16 +9,70 @@ class Solution {
         return 0;
     }
 
-    public static void bubbleSort(int arr[]) 
+    public static void merge(int arr[], int l, int m, int r) 
     { 
-        int n = arr.length; 
-        for (int i = 0; i < n - 1; i++) 
-            for (int j = 0; j < n - i - 1; j++) 
-                if (arr[j] > arr[j + 1]) { 
-                    int temp = arr[j]; 
-                    arr[j] = arr[j + 1]; 
-                    arr[j + 1] = temp; 
-                } 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        // Create temp arrays 
+        int L[] = new int[n1]; 
+        int R[] = new int[n2]; 
+  
+        // Copy data to temp arrays 
+        for (int i = 0; i < n1; ++i) 
+            L[i] = arr[l + i]; 
+        for (int j = 0; j < n2; ++j) 
+            R[j] = arr[m + 1 + j]; 
+  
+        // Merge the temp arrays 
+        // Initial indexes of first and second subarrays 
+        int i = 0, j = 0; 
+  
+        // Initial index of merged subarray array 
+        int k = l; 
+        while (i < n1 && j < n2) { 
+            if (L[i] <= R[j]) { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+  
+        // Copy remaining elements of L[] if any 
+        while (i < n1) { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+  
+        // Copy remaining elements of R[] if any 
+        while (j < n2) { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
+  
+    // Main function that sorts arr[l..r] using 
+    // merge() 
+    public static void sort(int arr[], int l, int r) 
+    { 
+        if (l < r) { 
+            // Find the middle point 
+            int m = (l + r) / 2; 
+  
+            // Sort first and second halves 
+            sort(arr, l, m); 
+            sort(arr, m + 1, r); 
+  
+            // Merge the sorted halves 
+            merge(arr, l, m, r); 
+        } 
     }
 
     public static boolean area(int a,int b, int c){
