6058130736787885fa1a7ffd2246a9190245bbd1
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-10 23:22:14 +0500

    Time: 783 ms (5.07%), Space: 43.9 MB (96.62%) - LeetHub

---
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -0,0 +1,33 @@
+class Solution {
+   public static int largestPerimeter(int[] nums) {
+        bubbleSort(nums);
+        for (int i = nums.length-1; i >= 2; i--) {
+            if (area(nums[i], nums[i-1], nums[i-2])) {
+                return nums[i]+nums[i-1]+nums[i-2];
+            }
+        }
+        return 0;
+    }
+
+    public static void bubbleSort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n - 1; i++) 
+            for (int j = 0; j < n - i - 1; j++) 
+                if (arr[j] > arr[j + 1]) { 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j + 1]; 
+                    arr[j + 1] = temp; 
+                } 
+    }
+
+    public static boolean area(int a,int b, int c){
+        if (a >= b+c || b >= a+c || c >= a+b) {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
