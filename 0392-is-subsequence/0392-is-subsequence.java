737de5d95521521228c715b643b2dd0504fdee08
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-17 00:19:49 +0500

    Time: 1 ms (91.22%), Space: 40.1 MB (91.14%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,8 +3,8 @@ class Solution {
         if(s.equals(t)) return true;
         if(s.equals("")) return true;
         int p1 = 0;
-        int size = t.length();
-        int size2 = s.length();
+        final int size = t.length();
+        final int size2 = s.length();
         for (int i = 0; i < size; i++) {
             if (t.charAt(i) == s.charAt(p1)) {
                 p1++;
