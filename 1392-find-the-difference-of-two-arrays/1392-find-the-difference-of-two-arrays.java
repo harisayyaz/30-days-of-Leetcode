e22f68ebb5b1d93b6cbe1009463c4a3b0faf86a4
Haris Ayyaz <106795373+harisxayyaz@users.noreply.github.com>
2023-10-18 02:00:47 +0500

    Time: 54 ms (16.27%), Space: 44.8 MB (20.71%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
@@ -1,36 +1,27 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
         List<Integer> list1 = new ArrayList<Integer>(0);
         List<Integer> list2 = new ArrayList<Integer>(0);
-        final int size1 = nums1.length;
-        final int size2 = nums2.length;
-        for (int i = 0; i < size1; i++) {
-            int check = 0;
-    
-            for (int j = 0; j < size2; j++) {
-                if (nums1[i]==nums2[j]) {
-                    check++;
-                    break;    
-                }
-            }
-            if (check==0 && !inList(list1, nums1[i])) {
 
-                list1.add(nums1[i]);                
-            }
-            
+        for (Integer integer : nums1) {
+            set1.add(integer);
         }
 
+        for (Integer integer : nums2) {
+            set2.add(integer);
+        }
 
-         for (int i = 0; i < size2; i++) {
-            int check = 0;
-            for (int j = 0; j < size1; j++) {
-                if (nums2[i]==nums1[j]) {
-                    check++;
-                    break;    
-                }
+        for (Integer integer : nums1) {
+            if (!set2.contains(integer) && !list1.contains(integer)) {
+                list1.add(integer);
             }
-            if (check==0 && !inList(list2, nums2[i])) {
-                list2.add(nums2[i]);                
+        }
+
+         for (Integer integer : nums2) {
+            if (!set1.contains(integer) && !list2.contains(integer)) {
+                list2.add(integer);
             }
         }
 
@@ -38,16 +29,5 @@ class Solution {
         fina.add(list1);
         fina.add(list2);
         return fina;
-        
-    }
-
-    public static boolean inList(List<Integer> list, int x)
-    {
-        for (Integer integer : list) {
-            if (integer==x) {
-                return true;
-            }
-        }
-        return false;
     }
 }
