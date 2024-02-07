2920d26af4ff7c1c45c4d885adbbffe40c0e107f
Haris Ayyaz <106795373+harisayyaz@users.noreply.github.com>
2024-02-07 12:15:14 +0500

    Time: 4 ms (83.12%) | Memory: 9 MB (9.29%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,13 +3,13 @@ public:
     bool isPalindrome(int x) {
     string str1 = to_string(x);
     int div = 10;
-    for (int i = 0; i < str1.size(); i++)
+    int size = str1.size();
+    for (int i = 0; i < size; i++)
     {
         int character = int(str1.at(i)) - 48;
         int end = x % div;
         if (character == end)
         {
-            cout << "here";
             x /= 10;
             continue;
         }
