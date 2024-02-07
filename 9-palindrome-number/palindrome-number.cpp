1c1e31d7fa4c2dccee76aeb7e2d0d8d002d8f6bd
Haris Ayyaz <106795373+harisayyaz@users.noreply.github.com>
2024-02-07 12:15:46 +0500

    Time: 7 ms (74.18%) | Memory: 8.9 MB (11.05%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     bool isPalindrome(int x) {
     string str1 = to_string(x);
-    int div = 10;
     int size = str1.size();
     for (int i = 0; i < size; i++)
     {
         int character = int(str1.at(i)) - 48;
-        int end = x % div;
+        int end = x % 10;
         if (character == end)
         {
             x /= 10;
